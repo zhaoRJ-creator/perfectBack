ae8bf76108f98c20d7244448f61664ebbf00d290
zhaoruijing <p_zhaoruijing@ledupeiyou.com>
2025-08-22 17:30:18 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,9 @@
       :rotation="25"
     />
     <div class="content">
+      <input type="text" class="input" />
       <TypeWriter
-        :text="'你好，我是打字机！'"
+        :text="str"
         :typingSpeed="200"
         :pauseDuration="1500"
         :className="['typeWriter']"
@@ -36,12 +37,17 @@
 // import TextType from "./components/text.vue";
 import TypeWriter from "./components/typeWriter.vue";
 import Back from "./components/back.vue";
+const str = "Radiant beams for creative user interfaces";
 </script>
 
-<style scoped>
+<style scoped lang="less">
 .typeWriter {
-  font-weight: bold;
-  color: pink;
+  text-align: center;
+  width: 80%;
+  margin-top: 200px;
+  font-size: 70px;
+  font-weight: 1000;
+  color: #fff;
 }
 .beams-container {
   width: 100%;
@@ -50,10 +56,25 @@ import Back from "./components/back.vue";
   overflow: hidden;
   background: #000;
 }
-.content{
+.content {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+
+  .input {
+    width: 70%;
+    height: 60px;
+    border: none;
+    border-radius: 30px;
+    padding: 5px;
+    opacity: 0.2;
+    margin-top: 80px;
+  }
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,38 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   base: "./",
+  // server: {
+  //   port: 8881,
+  //   open: true,
+  // },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/assets/style/element/index.scss" as *;`,
+      },
+    },
+  },
+  build: {
+    outDir: "dists",
+  },
+  optimizeDeps: {
+    exclude: ["element-plus"],
+  },
+  server: {
+    port: 8881,
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
